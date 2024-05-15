4a8654259c1fe848c2732826c27d779d6e934bc9
이강석 <65838532+cpfur18@users.noreply.github.com>
2024-05-15 11:45:25 +0900

    fix : Application.java

--- src/run/Application.java
+++ src/run/Application.java
@@ -3,12 +3,10 @@ package run;
 import notice.controller.NoticeController;
 
 public class Application {
-    public static void main(String[] args){
-        // 메인 실행 파일
 
-        // Test
-        NoticeController noticeController = new NoticeController();
-        noticeController.run();
+    public static void main(String[] args) {
+
+        new MemberMenu().mainMenu();
 
     }
 
