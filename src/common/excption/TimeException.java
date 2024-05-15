ae3e4678541a1c8f5a6064fd82059568f0d870b4
LKS <cpfur18@gmail.com>
2024-05-15 09:20:27 +0900

    feat : UI 구현 및 DB 연결 테스트

---
+++ src/common/excption/TimeException.java
@@ -0,0 +1,11 @@
+package common.excption;
+
+public class TimeException extends Exception{
+    public TimeException(String message) {
+        super(message);
+    }
+
+    public TimeException(){
+
+    }
+}
